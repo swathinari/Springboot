ba1b8d7a62e1fb9f6a84811ea4362ff1efc96382
Swathinarikadapa <132544482+swathinari@users.noreply.github.com>
2025-07-07 20:38:35 +0530

    java basic

---
+++ basic/basicTopics/src/com/java/basics/RuntimePoly.java
@@ -0,0 +1,38 @@
+package com.java.basics;
+
+// here i used static class because in the same class i extended the another class. if you are 
+//trying to override the same method you can use 2 different classes then extends it.
+
+public class RuntimePoly {
+	int res;
+	
+	public int add(int a, int b) {
+		
+		res=a+b;
+		return res;
+	}
+	
+	static class DynamicMethodDispatch extends RuntimePoly{
+		public int add(int a, int b) {
+			res= a+b;
+			return res;
+		}
+	}
+	static class Swathi extends DynamicMethodDispatch{
+		public int add(int a, int b) {
+			res= a*b;
+			return res;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RuntimePoly 	dynamicMethodDispatch = new DynamicMethodDispatch();
+		RuntimePoly swathi= new Swathi();
+		System.out.println("dynamic method dispatch: "+dynamicMethodDispatch.add(10, 100));
+		System.out.println("printing from swathi class: "+swathi.add(10, 10));
+	
+		
+	}
+
+}
